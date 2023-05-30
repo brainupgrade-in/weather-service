49bf27e6deb0f803aa2eb7e6b0587f68a0dca0f9
Rajesh G <rajesh@brainupgrade.in>
2023-05-30 09:46:43 +0530

    accuweather

--- src/main/java/in/brainupgrade/weather/RemoteApiFetcherImpl.java
+++ src/main/java/in/brainupgrade/weather/RemoteApiFetcherImpl.java
@@ -5,24 +5,39 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import in.brainupgrade.weather.model.currentWeather.City;
 import in.brainupgrade.weather.model.currentWeather.WeatherForecast;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class RemoteApiFetcherImpl implements RemoteApiFetcher {
 
+	@Value("${city.service.provider}")
+	private String cityURL;
+	@Value("${weather.service.provider}")
+	private String weatherURL;
+	RestTemplate restTemplate = new RestTemplate();
+
 	public Optional<WeatherForecast> fetchWeatherForecastFromRemoteApi(City city) {
 		log.info("Weather info requested for the city: " + city.getTitle());
-		String weatherUrl = "https://www.metaweather.com/api/location/" + city.getWoeid();
+		// String weatherUrl = "https://www.metaweather.com/api/location/" + city.getWoeid();
 		try {
-			RestTemplate restTemplateWeather = new RestTemplate();
-			Optional<WeatherForecast> result = Optional
-					.ofNullable(restTemplateWeather.getForObject(weatherUrl, WeatherForecast.class));
+			// Optional<WeatherForecast> result = Optional
+			// 		.ofNullable(restTemplateWeather.getForObject(weatherUrl, WeatherForecast.class));
+			ObjectNode result = restTemplate.getForObject(cityURL, ObjectNode.class, city.getWoeid());
+			// String address = result.get("results").get(0).get("formatted_address").textValue();
+			WeatherForecast weatherForecast =  new WeatherForecast();
+			weatherForecast.setLattLong(city.getLattLong());
+			weatherForecast.setSunRise("");
+
 			log.info(String.format("Returning the result for the city %s", city.getTitle()));
-			return result;
+			return null;
 		} catch (RestClientException ex) {
 			log.error("Error during fetching WeatherForecast from remote API.", ex);
 		} finally {
